5f9c63aa7b7bc969e0175f37cbcea8309ea30966
Vitaly02071979 <ver979@mail.ru>
2023-11-30 13:08:53 +0500

    типа делаю 303 лабу, вот...

---
+++ 2 курс, 2 семестр/works3/303/oopp/class.h
@@ -0,0 +1,10 @@
+/* 2018 ReVoL Primer Template */
+/* class.h */
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <assert.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <iostream>
+using namespace std;
