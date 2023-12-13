ebedfe256674d39293b30f7976f9393b3843a251
Sn0wled <misha.safin.sn0wled@gmail.com>
2023-12-13 09:47:07 +0500

    uml

---
+++ 3 курс, 1 семестр/works1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿A[] f = { new A(), new B(), new A() };
+
+foreach (A a in f)
+{
+    a.Show();
+}
+
+A a1 = new();
+B b1 = new();
+Console.WriteLine(a1.Equals(b1));
+
+class A
+{
+    int a = 10;
+    public virtual void Show() => Console.WriteLine("A");
+    public int HetHashCode()
+    {
+        return 0;
+    }
+    public override bool Equals(object? obj)
+    {
+        if (obj is A a)
+        {
+            return this.a == a.a;
+        }
+        return false;
+    }
+}
+
+class B : A {
+}
